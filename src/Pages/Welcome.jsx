8974371cbb810c935b51bd4ff8ab3829c4520f51
sashaharmer@yahoo.com <sashaharmer@yahoo.com>
2023-01-26 00:01:03 -0500

    fixed bubble text aligment

--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -11,27 +11,48 @@ const Welcome = () => {
   return (
     <div className="App">
   
-      <div className="d-flex justify-content-evenly vh-100">        
-        <div className="col-3 mt-5">
-          <img src="static/me.png" className="img-fluid"  alt="pic_of_me" style={{height:"80vh"}}/>
+      <div className="d-flex justify-content-evenly vh-100">    
+          
+        <div className="col-4 mt-2">
+            <div className="d-flex align-items-center justify-content-center vh-100">
+              <img src="static/me.png" className="img-fluid"  alt="pic_of_me" style={{height:"80vh"}}/>
+            </div>
         </div>
 
-        <div className="col-3 p-2 mt-4 vw-25" style={{height:"40vh"}}>        
-            <div className="bubble bubble-bottom-left" >
+        {/* <div className="col-3 mt-4" style={{height:"40vh",border:"3px solid red"}}>        
+            <div className="bubble bubble-bottom-left">
               <p className="text-wrap" ref={ref} style={{ fontSize, height: "100%", width: "auto" }} >
                 Hi there! Welcome to my Portfolio! I'm Aleksandra/Sasha, Software Engineer with background in math. Here I'm sharing with you my true passion - Web Development! I hope you enjoy
                 getting to know me...                  
               </p>             
             </div>         
-        </div>      
+        </div>       */}
 
-        <div className="col-3 d-flex flex-column justify-content-evenly m-5" style={{ fontSize:"7vh"}}>
-          <div style={{border:"3px solid black", backgroundColor:"white", borderRadius:"10px", }}><Link to={"/about"}  style={{textDecoration:"none", color:"black"}}>About </Link></div>
-          <div style={{border:"3px solid black", backgroundColor:"white", borderRadius:"10px", }}><Link to={"/projects"}  style={{textDecoration:"none", color:"black"}}>Projects</Link></div>
-          <div style={{border:"3px solid black", backgroundColor:"white", borderRadius:"10px", }}><Link to={"/contact"}  style={{textDecoration:"none", color:"black"}}>Contact</Link></div>
-          <div style={{border:"3px solid black", backgroundColor:"white", borderRadius:"10px", }}><Link to={"/resume"}  style={{textDecoration:"none", color:"black"}}>Resume</Link></div>
+           
+
+        <div className="col-8 d-flex flex-column justify-content-evenly mt-2" style={{ fontSize:"7vh"}}>
+          <div className="d-flex justify-content-start " style={{height:"30%", width:"50%"}}>
+            <div className="bubble bubble-bottom-left">
+                <p className="text-wrap" ref={ref} style={{ fontSize, height: "100%", width: "auto" }} >
+                  Hi there! Welcome to my Portfolio! I'm Aleksandra/Sasha, Software Engineer with background in math. Here I'm sharing with you my true passion - Web Development! I hope you enjoy
+                  getting to know me...                  
+                </p>             
+            </div> 
+          </div>
+            
+        <div className="d-flex flex-column align-self-end mx-5 justify-content-evenly" style={{height:"60%", width:"45%"}}>
+        {/* className="d-flex align-self-end justify-content-center mx-5" */}
+
+
+          <Link to={"/about"}  style={{textDecoration:"none", color:"black",fontSize:"80%"}}><div class="border border-dark border-4 bg-light rounded-4">About </div></Link>
+          
+          <Link to={"/projects"} style={{textDecoration:"none", color:"black", fontSize:"80%"}}><div class="border border-dark border-4 bg-light rounded-4">Projects</div></Link>
+
+          <Link to={"/contact"}  style={{textDecoration:"none", color:"black", fontSize:"80%"}}><div class="border border-dark border-4 bg-light rounded-4">Contact</div></Link>
+          
+          <Link to={"/resume"}  style={{textDecoration:"none", color:"black", fontSize:"80%"}}><div class="border border-dark border-4 bg-light rounded-4">Resume</div></Link>
         </div>
-
+        </div>
       </div>      
     </div>
   );
--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import styled from 'styled-components'
-
-// let PictureBox = styled.div`
-//      width: 300px;
-//      transform: rotate(-15deg);
-
-// `
 
 const Pictures = () => {
   return (
-    // className='col-md-6 float-md-start mb-3 ms-md-5'
-    // <PictureBox>
-<div className='container ms-1' >
-        <img src="https://i.imgur.com/Yqmjbhq.jpg" className="img-thumbnail rounded-5  " alt="selfie" style={{width:"300px", rotate:"-10deg"}}></img>
+
+<div className='container ms-3' >
+        <img src="https://i.imgur.com/Yqmjbhq.jpg" className="img-thumbnail rounded-5  " alt="selfie" style={{ width:"350px", rotate:"-10deg"}}></img>
 </div>
-    // {/* </PictureBox> */}
 
   )
 }
