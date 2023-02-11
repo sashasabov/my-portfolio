9c8d7f73f5168375197db8c8504ee94f90ae3691
sashaharmer@yahoo.com <sashaharmer@yahoo.com>
2023-02-11 16:35:20 -0500

    cleaned Resume page

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,48 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import '../App.css';
 
 const Resume = () => {
 
-    // const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess() {
-    // setNumPages(numPages);
-    // setPageNumber(1);
-  } 
-
-  //{ numPages }
-
-// function changePage(offSet){
-//     setPageNumber(prevPageNumber =>prevPageNumber +offSet)}
-
-// function changePageBack(){
-// changePage(-1)
-// }
-
-// function changePageNext(){
-// changePage(+1)
-// }
   return (
-    <div className="App" style={{height:"100vh"}}>
-    <div className='d-flex flex-column align-items-center mt-3'>
-        {/* <p>
-        Page {pageNumber} of {numPages}
-      </p>
-      {pageNumber > 1 && 
-      <button onClick={changePageBack} type="button" class="btn btn-dark">Previous Page</button>
-      }
-      {
-      pageNumber < numPages &&
-      <button onClick={changePageNext} type="button" class="btn btn-dark">Next Page</button>
-      } */}
-      <Document file="static/Resume A. Sabov.pdf" onLoadSuccess={onDocumentLoadSuccess} >
-       <Page pageNumber={pageNumber} className="border border-dark border-4 rounded bg-white"/>
-        
-      </Document>
-      
-    </div>
+    <div className="App" >
+      <div className='d-flex flex-column align-items-center mt-3'>      
+        <Document file="static/Resume A. Sabov.pdf">
+        <Page pageNumber={1}/>
+        </Document>     
+      </div>
     </div>
   )
 }
